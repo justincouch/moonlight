a80f9e178652491a54d2a6d03df10c717b08aeed
Justin Couch <justincouchdesign@gmail.com>
2021-07-29 08:56:12 -0400

    logging2

--- moonlight.py
+++ moonlight.py
@@ -15,7 +15,7 @@ logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBU
 logging.debug('This message should go to the log file')
 logging.info('So should this')
 logging.warning('And this, too')
-logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+logging.error('And non-ASCII stuff, too')
 
 GPIO.setmode(GPIO.BCM)
 
